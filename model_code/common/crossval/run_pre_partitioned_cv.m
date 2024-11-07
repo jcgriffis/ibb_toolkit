89c65c684a9d49614ae319697f8b462f6389197f
jcgriffis <jcgriffis@uiowa.edu>
2024-11-07 12:05:15 -0600

    Add files via upload

--- model_code/common/crossval/run_pre_partitioned_cv.m
+++ model_code/common/crossval/run_pre_partitioned_cv.m
@@ -45,35 +45,9 @@ for i = 1:cfg.cv.repeats % Loop over CV repeats
                 clear y_pred_confound
             end
         end
-
         % Apply standardization if specified
-        if cfg.standardize == 1  % Convert X to z-scores
-            miss_col = sum(x_train,1)==0;
-            [x_train, cfg.Cx, cfg.Sx] = normalize(x_train);
-            x_train(:,miss_col)=0; % Since normalize will cause columns to become NaN if they are all 0
-            x_test = normalize(x_test, 'center', cfg.Cx, 'scale', cfg.Sx);
-            x_test(:,miss_col)=0; % Since normalize will cause columns to become NaN if they have SD = 0 in train set
-        elseif cfg.standardize == 2 % Convert Y to z-scores 
-            if cfg.cat_Y == 1
-                cfg.standardize = 0;
-                disp('Unable to standardize categorical Y, setting standardize flag to 0');    
-            else
-                [y_train, cfg.Cy, cfg.Sy] = normalize(y_train);
-                y_test = normalize(y_test, 'center', cfg.Cy, 'scale', cfg.Sy);
-            end
-        elseif cfg.standardize == 3 % Convert X and Y to z-scores
-            miss_col = sum(x_train,1)==0;
-            [x_train, cfg.Cx, cfg.Sx] = normalize(x_train);
-            x_train(:,miss_col)=0; % Since normalize will cause columns to become NaN if they are all 0
-            x_test = normalize(x_test, 'center', cfg.Cx, 'scale', cfg.Sx);
-            x_test(:,miss_col)=0; % Since normalize will cause columns to become NaN if they have SD = 0 in train set
-            if cfg.cat_Y == 0
-                disp('Unable to standardize categorical Y, setting standardize flag to 1');
-                cfg.standardize = 0;
-            else
-                [y_train, cfg.Cy, cfg.Sy] = normalize(y_train);
-                y_test = normalize(y_test, 'center', cfg.Cy, 'scale', cfg.Sy);      
-            end
+        if cfg.standardize > 0  
+           [cfg, x_train, x_test, y_train, y_test] = rescale_dataset(cfg, x_train, x_test, y_train, y_test);
         end
     
         % Run inner loop and get cross-validation results
