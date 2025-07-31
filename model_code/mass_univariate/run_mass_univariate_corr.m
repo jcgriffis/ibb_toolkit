c1b14c6ca91c204afbf6cc49c6cd759776e54611
jcgriffis <jcgriffis@uiowa.edu>
2025-07-31 14:27:18 -0500

    Add files via upload
    Updated files to perform partial correlations instead of confound regression and to accommodate Spearman correlations

--- model_code/mass_univariate/run_mass_univariate_corr.m
+++ model_code/mass_univariate/run_mass_univariate_corr.m
@@ -1,22 +1,43 @@
-function [model_results] = run_mass_univariate_corr(X, Y, model_results)
+function [model_results] = run_mass_univariate_corr(X, Y, cfg, model_results)
 
 % Run mass-univariate correlation analyses
 % Joseph Griffis 2024
 
 % Run mass univariate correlations
-[model_results.coeff, model_results.coeff_pvals] = corr(X, Y);
+if ~isfield(cfg, 'cor_type')
+    cor_type = 'Pearson';
+else
+    cor_type = cfg.cor_type;
+end
+if isempty(cfg.confounds)
+    disp('No confounds specified...running bivariate correlations')
+    [model_results.coeff, model_results.coeff_pvals] = corr(X, Y, 'type', cor_type);
+else
+    disp('Confounds specified...running partial correlations')
+    [model_results.coeff, model_results.coeff_pvals] = partialcorr(X, Y, cfg.confounds, 'type', cor_type);
+end
 
 % Correct voxel-level p-values if indicated
 model_results.coeff_vfwe_pvals = bonf_holm(model_results.coeff_pvals);
 model_results.coeff_vfdr_pvals = mafdr(model_results.coeff_pvals, 'BHFDR', true);
 
 % Get t-statistics
-model_results.tstat = model_results.coeff ./ sqrt((1-model_results.coeff.^2) ./ (length(Y)-2));
+if isempty(cfg.confounds)
+    model_results.tstat = model_results.coeff ./ sqrt((1-model_results.coeff.^2) ./ (length(Y)-2));
+else
+    model_results.tstat = model_results.coeff .* sqrt((length(Y)-2-size(cfg.confounds,2)) ./ (1-model_results.coeff.^2));
+end
 
-% Generate CPM model
-model_results.obs_y = Y;
-cpm_train = X*model_results.coeff;
-mdl = fitlm(cpm_train, Y);
-model_results.cpm_mdl = mdl;
+if isempty(cfg.confounds)
+    % Generate CPM model
+    model_results.obs_y = Y;
+    cpm_train = X*model_results.coeff;
+    if numel(unique(Y)) > 2
+        mdl = fitlm(cpm_train, Y);
+    else
+        mdl = fitlm(Y, cpm_train, 'CategoricalVars', 'x1');
+    end
+    model_results.cpm_mdl = mdl;
+end
 
 end
--- model_code/mass_univariate/run_perm_mass_uni_corr.m
+++ model_code/mass_univariate/run_perm_mass_uni_corr.m
@@ -4,6 +4,11 @@ function [model_results] = run_perm_mass_uni_corr(X, Y, cfg, model_results)
 % Joseph Griffis 2024
 
 % Get permuted Y
+if ~isempty(cfg.confounds)
+    mdl = fitlm(cfg.confounds, Y);
+    Y = mdl.Residuals.Raw;
+end
+
 n_perm = cfg.perm.n_perm;
 perm_Y = zeros(length(Y), n_perm);        
 for i = 1:n_perm
@@ -14,18 +19,40 @@ end
 t_perm = zeros(size(X,2), n_perm);
 
 % Run permutations
-if cfg.parallel == 1
-    parfor i = 1:n_perm
-        disp(['Running permutation analysis: ' num2str(i)]);
-        r = corr(X, perm_Y(:,i));
-        t_perm(:,i) = r ./ sqrt((1-r.^2) ./ (length(Y)-2));
+if ~isfield(cfg, 'cor_type')
+    cor_type = 'Pearson';
+else
+    cor_type = cfg.cor_type;
+end
+if isempty(cfg.confounds)
+    if cfg.parallel == 1
+        parfor i = 1:n_perm
+            disp(['Running permutation analysis: ' num2str(i)]);
+            r = corr(X, perm_Y(:,i), 'type', cor_type);
+            t_perm(:,i) = r ./ sqrt((1-r.^2) ./ (length(Y)-2));
+        end
+    else
+        for i = 1:n_perm
+            disp(['Running permutation analysis: ' num2str(i)]);
+            r = corr(X, perm_Y(:,i), 'type', cor_type);
+            t_perm(:,i) = r ./ sqrt((1-r.^2) ./ (length(Y)-2));
+        end
     end
 else
-    for i = 1:n_perm
-        disp(['Running permutation analysis: ' num2str(i)]);
-        r = corr(X, perm_Y(:,i));
-        t_perm(:,i) = r ./ sqrt((1-r.^2) ./ (length(Y)-2));
-    end
+    confounds = cfg.confounds;
+    if cfg.parallel == 1
+        parfor i = 1:n_perm
+            disp(['Running permutation analysis: ' num2str(i)]);
+            r = partialcorr(X, perm_Y(:,i), confounds, 'type', cor_type);
+            t_perm(:,i) = r .* sqrt((length(Y)-2-size(confounds,2)) ./ (1-r.^2));
+        end
+    else
+        for i = 1:n_perm
+            disp(['Running permutation analysis: ' num2str(i)]);
+            r = partialcorr(X, perm_Y(:,i), confounds, 'type', cor_type);
+            t_perm(:,i) = r .* sqrt((length(Y)-2-size(confounds,2)) ./ (1-r.^2));
+        end
+    end    
 end
 
 % Get permutation results
