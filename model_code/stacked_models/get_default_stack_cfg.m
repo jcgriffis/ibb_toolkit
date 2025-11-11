9c678e74f872cebc437aaa6cc6d07140ac0273ca
jcgriffis <jcgriffis@uiowa.edu>
2025-11-11 09:33:59 -0600

    Update get_default_stack_cfg.m

--- model_code/stacked_models/get_default_stack_cfg.m
+++ model_code/stacked_models/get_default_stack_cfg.m
@@ -35,7 +35,7 @@ switch cfg.model_spec
         end        
         cfg.standardize = 0;
     case {'lasso', 'ridge', 'rlinsvr', 'logistic_lasso', 'logistic_ridge', 'rlinsvc'} % Regularized linear models
-        if contains(cfg.model_spec, {'lasso', 'ridge'})
+        if contains(cfg.model_spec, {'lasso', 'ridge'}) && ~contains(cfg.model_spec, 'logistic')
             cfg.learner = 'leastsquares'; % OLS regression
         elseif contains(cfg.model_spec, {'logistic'})
             cfg.learner = 'logistic';
