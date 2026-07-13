d9e1f4ee9ba5316bf9d9688f15319cd0a057c8b2
jcgriffis <jcgriffis@uiowa.edu>
2026-07-13 15:28:17 -0500

    Add files via upload

--- model_code/common/explanatory/boot/run_bootstrap_models.m
+++ model_code/common/explanatory/boot/run_bootstrap_models.m
@@ -19,7 +19,6 @@ if isfield(model_results, 'Cy')
 end
 
 % Standardization
-standardize = cfg.standardize;
 if cfg.standardize > 0 && ~isfield(cfg, 'standardize_method')
     cfg.standardize_method = 'zscore';
     cfg.standardize_type = 'std';
@@ -33,84 +32,45 @@ elseif cfg.standardize == 0
     cfg.standardize_method = [];
     cfg.standardize_type = [];
 end
-method = cfg.standardize_method;
-type = cfg.standardize_type;
 
 %%% Specify bootstrap analysis parameters based on modeling approach
 switch cfg.model_spec
     case 'plsr'
-        opt_k = model_results.opt_k; % Optimized component number
-        bootfun = @(IV,DV) run_boot_plsr(IV,DV,opt_k,standardize, method, type); 
+        bootfun = @(IV,DV) run_boot_plsr(IV,DV,model_results.opt_k,cfg.standardize, cfg.standardize_method, cfg.standardize_type); 
     case 'pls_da'
-        opt_k = model_results.opt_k; % Optimized component number
-        cost = cfg.cost;
-        bootfun = @(IV,DV) run_boot_plsda(IV,DV,opt_k,cost,standardize, method, type); 
+        bootfun = @(IV,DV) run_boot_plsda(IV,DV, model_results.opt_k, cfg.cost, cfg.standardize, cfg.standardize_method, cfg.standardize_type); 
     case {'ridge', 'lasso', 'rlinsvr'}
-        reg_type = cfg.reg_type;
-        lambda = model_results.Lambda;
-        learner = cfg.learner;       
-        if strcmp(learner, 'svm') == 1
-            learner = 1;
-        else
-            learner = 0;
-        end
-        if strcmp(reg_type, 'lasso') == 1
-            reg_type = 1;
-        else
-            reg_type = 0;
-        end
-        bootfun = @(IV, DV) run_boot_regmdl(IV, DV, lambda, learner, reg_type, standardize, method, type); 
+        bootfun = @(IV, DV) run_boot_regmdl(IV, DV, model_results.Lambda, cfg.learner, cfg.reg_type, cfg.standardize, cfg.standardize_method, cfg.standardize_type); 
     case {'logistic_ridge', 'logistic_lasso', 'rlinsvc'}
-        reg_type = cfg.reg_type;
-        lambda = model_results.Lambda;
-        learner = cfg.learner;       
-        cost = cfg.cost;
-        if strcmp(learner, 'svm') == 1
-            learner = 1;
-        else
-            learner = 0;
-        end
-        if strcmp(reg_type, 'lasso') == 1
-            reg_type = 1;
-        else
-            reg_type = 0;
-        end
-        bootfun = @(IV, DV) run_boot_classmdl(IV, DV, lambda, learner, reg_type, cost, standardize, method, type);
+        bootfun = @(IV, DV) run_boot_classmdl(IV, DV, model_results.lambda, cfg.learner, cfg.reg_type, cfg.cost, cfg.standardize, cfg.standardize_method, cfg.standardize_type);
     case {'linsvr', 'kernsvr'}
-        C = model_results.C;
-        gamma = model_results.gamma;
-        if strcmp(cfg.kernel, 'linear') == 1
-            kernel = 0;
-        elseif strcmp(cfg.kernel, 'rbf') == 1
-            kernel = 1;
-        end        
-        bootfun = @(IV,DV) run_boot_svr(IV,DV,C,gamma,kernel,standardize, method, type); 
+        bootfun = @(IV,DV) run_boot_svr(IV,DV,model_results.C,model_results.gamma,cfg.kernel,model_results.epsilon,cfg.standardize,cfg.standardize_method, cfg.standardize_type); 
     case {'linsvc', 'kernsvc'}
-        C = model_results.C;
-        gamma = model_results.gamma;
-        standardize = cfg.standardize;
-        cost = cfg.cost;
-        if strcmp(cfg.kernel, 'linear') == 1
-            kernel = 0;
-        elseif strcmp(cfg.kernel, 'rbf') == 1
-            kernel = 1;
-        end
-        bootfun = @(IV,DV) run_boot_svc(IV,DV,C,gamma,kernel,standardize, cost, method, type);
+        bootfun = @(IV,DV) run_boot_svc(IV,DV,model_results.C,model_results.gamma,cfg.kernel,cfg.standardize,cfg.cost,cfg.standardize_method,cfg.standardize_type);
 end
 
 % Weight observations by stratification groups if applicable
-if isfield(cfg, 'strat_groups')
+if isfield(cfg, 'strat_groups') && isfield(cfg.boot, 'weighted')
 
-    % Get bootstrap weights
-    strat_groups = unique(cfg.strat_groups);
-    w = zeros(size(cfg.strat_groups));
+    if cfg.boot.weighted == 1
 
-    for i = 1:length(strat_groups)
+        % Get bootstrap weights
+        strat_groups = unique(cfg.strat_groups);
+        w = zeros(size(cfg.strat_groups));
+    
+        for i = 1:length(strat_groups)
+    
+            p = numel(cfg.strat_groups(cfg.strat_groups==strat_groups(i)))./numel(cfg.strat_groups); % prevalence of group
+            w1 = (1./length(cfg.strat_groups)) ./ p; % weights for group
+            w(cfg.strat_groups==strat_groups(i)) = w1./length(strat_groups); % divide by N so weights sum to 1 
+    
+        end
+        
+    else
 
-        p = numel(cfg.strat_groups(cfg.strat_groups==strat_groups(i)))./numel(cfg.strat_groups); % prevalence of group
-        w1 = (1./length(cfg.strat_groups)) ./ p; % weights for group
-        w(cfg.strat_groups==strat_groups(i)) = w1./length(strat_groups); % divide by 2 so weights sum to 1 
-
+        % Default weighting
+        w = ones(length(Y),1)./length(Y);        
+        
     end
 
 else
